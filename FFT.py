45e347ae843e83482aa204c9e248e722a156c45e
maxgotts <max.gotts@gmail.com>
2019-04-30 09:59:04 -0700

    -

--- FFT.py
+++ FFT.py
@@ -1,14 +1,15 @@
 # FFT Definition
 
 def Fourier(subjects): # I: subject * time * ROI
+    subject_fft = []
     for timeset in subjects:
         np.transpose(timeset)
-
         x = [0 for i in range(len(timeset))]
         for ROI in range(len(timeset)):
             ROItimeset = timeset[ROI]
             N = len(timeset)
             for i in range(N-1)
-                x[ROI] += x_i*np.exp(2*np.pi*i/N) #x_n*e^(-i2pikn/N)
-
-            
+                for x_i in ROItimeset:
+                    x[ROI] += x_i*np.exp(2*np.pi*i/N)
+        subject_fft.append(x)
+    return subject_fft
