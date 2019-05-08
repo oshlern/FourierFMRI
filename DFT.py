dd93aa6fbc67ce8d1718ccee47ece2b1b5bc3a55
Max Gotts <spamgotts@gmail.com>
2019-05-08 16:13:56 -0700

    -

--- DFT.py
+++ DFT.py
@@ -21,22 +21,21 @@ import matplotlib.pyplot as mpl
 def DFT(x, N=10): # x is the data, N is buckets... I wrote it like this to better mimic the standard DFT equation notation
     X = np.arange(N)*0
     for n in range(N):
+        # Loop through to define each X_n 
         for k in range(len(x)): # k is a discrete timestep variable, data[k] is the data at this point
+            # For some X_n, sum up the value for each time step k
             X[n] += x[k] * np.exp(-1j*2*np.pi*k*n/N) # \sum_{k=0}^{M-1} x_k^{-i\frac{2pi}{N}\cdot kn} where M is the length of the signal
     return X
 
 def convertToMagPhase(freqs, d=2):
-    try:
-        new = []
-        perc = 10 ** d
-        for i in range(len(freqs[0][0])):
-            mag, phase = np.abs(freqs[0][0][i]), np.angle(freqs[0][0][i])
-            if mag > 0.001:
-                pass#print(i, mag, phase)
-            new.append((i, (int(mag*perc)/perc, int(phase*perc)/perc)))
-        return new
-    except IndexError:
-        return convertToMagPhase([[freqs]])
+    new = []
+    perc = 10 ** d
+    for i in range(len(freqs[0][0])):
+        mag, phase = np.abs(freqs[0][0][i]), np.angle(freqs[0][0][i])
+        if mag > 0.001:
+            pass#print(i, mag, phase)
+        new.append((i, (int(mag*perc)/perc, int(phase*perc)/perc)))
+    return new
 
 def convertToSin(freqs):
     sin = np.sin(0)
