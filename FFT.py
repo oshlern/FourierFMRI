799399477fd4888492082f86fa5cf32bb3d411df
Max Gotts <spamgotts@gmail.com>
2019-05-08 21:28:08 -0700

    -

--- FFT.py
+++ FFT.py
@@ -1,21 +1,6 @@
 # FFT Function
 import numpy as np
 
-# def Fourier(subjects): # I: subject * time * ROI
-#     subject_fft = []
-#     for timeset in subjects:
-#         np.transpose(timeset)
-#         x = [0 for i in range(len(timeset))]
-#         for ROI in range(len(timeset)):
-#             ROItimeset = timeset[ROI]
-#             N = len(timeset)
-#             for i in range(N-1):
-#                 for x_i in ROItimeset:
-#                     x[ROI] += x_i*np.exp(2*np.pi*i/N)
-#         subject_fft.append(x)
-#     return subject_fft
-
-
 
 def fft(data): # given list of values find dft
     N = len(data)
